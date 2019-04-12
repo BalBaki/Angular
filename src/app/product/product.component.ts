a6286f8e7b5f57c4c20ae7b22d66e5d420e3c778
Baki BAL <bakibal1994@gmail.com>
2019-04-12 19:04:24 +0300

    router yapısı eklendi

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
+import { AlertifyService } from '../services/alertify.service'
+import { ProductService } from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  providers:[ProductService]
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private alertifyService: AlertifyService,
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute
+
+  ) { }
   title = "Ürün Listesi";
-  products : Product[]=[
-    {id : 1, name : "Laptop", price : 150, categoryId : 1, description : "Monster Laptop",imageUrl:"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"},
-    {id : 2, name : "Mouse", price : 10, categoryId : 2, description : "A4 Tech",imageUrl:"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"},
-    {id : 1, name : "Laptop", price : 150, categoryId : 1, description : "Monster Laptop",imageUrl:"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"},
-    {id : 2, name : "Mouse", price : 10, categoryId : 2, description : "A4 Tech",imageUrl:"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"},
-    {id : 1, name : "Laptop", price : 150, categoryId : 1, description : "Monster Laptop",imageUrl:"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"},
-    {id : 2, name : "Mouse", price : 10, categoryId : 2, description : "A4 Tech",imageUrl:"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"}
+  filterText = "";
+  products: Product[];
 
 
-  ]
-  ngOnInit() {
+  addToCart(product) {
+    this.alertifyService.success(product.name + "added");
+
   }
 
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params=>{
+      this.productService.getProducts(params["categoryId"]).subscribe(data=>{
+        this.products=data
+      })
+    })
+  }
+  
 }
